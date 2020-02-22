94b6ad2a3d056eade790324ba1e36db3661dce6a
Tucker Vassau <tuckv@hotmail.com>
2020-02-22 00:55:28 -0700

    Connection to db set up

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // Dependencies
-var inquirer = require("inquirer");
-var mysql = require("mysql");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+});
